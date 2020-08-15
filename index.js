084f8808ee9f3e312cf11fb1f9f41e0febfead44
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-08-16 00:32:24 +0900

    tabList 구현

---
+++ index.js
@@ -0,0 +1,15 @@
+let tabLists = document.querySelectorAll("#about .tabList li");
+let tabContents = document.querySelectorAll("#about .tabContent li");
+
+tabLists.forEach((item,index)=>{
+    item.addEventListener("click",e=>{
+        tabLists.forEach(tab=>{
+            tab.classList.remove("on")
+        })
+        tabContents.forEach(item=>{
+            item.classList.remove("on");
+        })
+        tabLists[index].classList.add("on");
+        tabContents[index].classList.add("on");
+    })
+})
