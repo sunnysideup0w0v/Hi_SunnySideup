1cfa9b865808f36b48968afa0f755bb8a99a58a7
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-08-16 18:22:59 +0900

    prevent scroll
    modal창이 popup 되어있을 때 scroll이 되는 것을 막고, overflow hidden하면서 발생하는 width 크기 차이가 변환되도록 함.

--- index.js
+++ index.js
@@ -16,15 +16,18 @@ tabLists.forEach((item,index)=>{
 
 let images = document.querySelectorAll("#about .tabContent li .images li img");
 let modal = document.querySelector(".modal");
+let body = document.querySelector("body")
 images.forEach(img=>{
     console.log(img.src)
     img.addEventListener("click",e=>{
         let modalImg = document.querySelector(".modal .imgBox img");
         modal.classList.add("on");
+        body.classList.add("lock")
         modalImg.src = img.src;
     })
 })
 let btnClose = document.querySelector(".modal .btnClose");
 btnClose.addEventListener("click",()=>{
-    modal.classList.remove("on")
+    modal.classList.remove("on");
+    body.classList.remove("lock");
 })
